d3cf2f550d0791e7c8fecabb4fb743776c1a631b
Adhil Salim <bizadhil@gmail.com>
2022-12-25 08:31:17 +0530

    comments added in palindrome

--- Check whether a string is palindrome/Inheritance/Exception handling/Read line of integers and sum of all integers/Palindrome.java
+++ Check whether a string is palindrome/Inheritance/Exception handling/Read line of integers and sum of all integers/Palindrome.java
@@ -3,19 +3,24 @@ import java.util.*;
 class Palindrome {
     public static void main(String[] args) {
 
+        // Initializing variables
         String text, textCopy = "";
 
         Scanner scan = new Scanner(System.in);
 
+        // Getting string from user
         System.out.print("Enter String : ");
         text = scan.nextLine();
 
+        // Closing scanner
         scan.close();
 
+        // Copying string in reverse order
         for (int i = text.length() - 1; i >= 0; i--) {
             textCopy += text.charAt(i);
         }
 
+        // Checking for palindrome
         if (text.equals(textCopy)) {
             System.out.println("\nThe given string " + text + " is palindrome.");
         } else {
