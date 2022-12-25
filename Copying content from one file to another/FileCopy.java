8e357d304e19260ce6e9f3fadaa2e516dbdb579c
Adhil Salim <bizadhil@gmail.com>
2022-12-25 09:15:29 +0530

    Create FileCopy.java

---
+++ Copying content from one file to another/FileCopy.java
@@ -0,0 +1,47 @@
+import java.util.*;
+import java.io.*;
+
+class FileCopy {
+    public static void main(String[] args) {
+
+        FileInputStream fin;
+        FileOutputStream fout;
+        Scanner scan = new Scanner(System.in);
+
+        // Getting the name of file to read and to copy to
+        System.out.println("Enter the name of the file to read from: ");
+        String readFrom = scan.nextLine();
+
+        System.out.println("Enter the name of the file to write to: ");
+        String writeTo = scan.nextLine();
+
+        // Removing the .txt extension from the file names if the user entered it
+        readFrom = readFrom.replace(".txt", "");
+        writeTo = writeTo.replace(".txt", "");
+
+        // Closing the scanner
+        scan.close();
+
+        try {
+            fin = new FileInputStream(readFrom + ".txt");
+            fout = new FileOutputStream(writeTo + ".txt");
+
+            System.out.println("Copying content from " + readFrom + ".txt to " + writeTo + ".txt");
+
+            // Reading and Writing the content of the file
+            int data = fin.read();
+            while (data != -1) {
+                fout.write((char) data);
+                data = fin.read();
+            }
+
+            // Closing the streams
+            fin.close();
+            fout.close();
+
+            System.out.println("File copied successfully!");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
