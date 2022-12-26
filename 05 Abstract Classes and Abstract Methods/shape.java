e71350420ec0f4ea3e887e64b352fc2b17399d71
Adhil Salim <bizadhil@gmail.com>
2022-12-26 11:36:22 +0530

    renamed files

---
+++ 05 Abstract Classes and Abstract Methods/shape.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+abstract class shape {
+    abstract void numberOfSide();
+}
+
+class triangle extends shape {
+    void numberOfSide() {
+        System.out.println("size is 3");
+    }
+}
+
+class rectangle extends shape {
+    void numberOfSide() {
+        System.out.println("size is 4");
+    }
+}
+
+class hexagon extends shape {
+    void numberOfSide() {
+        System.out.println("size is 6");
+    }
+}
+
+class AbstractClass {
+    public static void main(String[] args) {
+
+        triangle tri = new triangle();
+        rectangle rect = new rectangle();
+        hexagon hexa = new hexagon();
+
+        shape s; // reference to shape
+
+        s = rect; // rectangle
+        s.numberOfSide();
+
+        s = tri; // triangle
+        s.numberOfSide();
+
+        s = hexa; // hexagon
+        s.numberOfSide();
+    }
+
+}
