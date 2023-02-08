b9e4ef0efbd92757df2dc20c85581b76cd2dea40
Adhil Salim <69722917+adhilsalim@users.noreply.github.com>
2023-02-08 21:11:36 +0530

    Update SyncThread.java
    The first program was not actually showing the usage/importance of synchronization.

--- 09 Thread/SyncThread.java
+++ 09 Thread/SyncThread.java
@@ -1,22 +1,25 @@
-import java.io.*;
-
-class newThread extends Thread {
+class myThread implements Runnable {
     synchronized public void run() {
-        for (int i = 0; i <= 5; i++) {
-            try {
-                System.out.println(Thread.currentThread().getName() + " " + i);
+        try {
+            for (int i = 0; i < 5; i++) {
+                System.out.println(Thread.currentThread().getName() + " is running");
                 Thread.sleep(1000);
-            } catch (InterruptedException e) {
             }
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 }
 
-class SyncThread {
-    public static void main(String[] args) {
-        newThread t1 = new newThread();
-        newThread t2 = new newThread();
-        newThread t3 = new newThread();
+public class multithreading {
+    public static void main(String[] args) throws Exception {
+        myThread mt = new myThread();
+
+        // to create multiple thread
+        Thread t1 = new Thread(mt, "Thread 0");
+        Thread t2 = new Thread(mt, "Thread 1");
+        Thread t3 = new Thread(mt, "Thread 2");
+
         t1.start();
         t2.start();
         t3.start();
