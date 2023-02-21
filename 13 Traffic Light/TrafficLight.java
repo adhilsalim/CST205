4bd206f57d711687f98023178413ca26ff2d1bee
Adhil Salim <bizadhil@gmail.com>
2023-02-21 10:56:30 +0530

    Create TrafficLight.java

---
+++ 13 Traffic Light/TrafficLight.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TrafficLight extends JPanel implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
