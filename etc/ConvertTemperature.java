5168feda40b13fda7e423085b3ab4f4e8cba539c
Adhil Salim <bizadhil@gmail.com>
2023-02-21 09:54:39 +0530

    Create ConvertTemperature.java

---
+++ etc/ConvertTemperature.java
@@ -0,0 +1,10 @@
+//package etc;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class ConvertTemperature implements ActionListener {
+
+    public void actionPerformed(ActionEvent e) {
+    }
+}
