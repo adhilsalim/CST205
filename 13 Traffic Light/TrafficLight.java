9dd5a883aa21d1758136588527efb8789b633790
Adhil Salim <bizadhil@gmail.com>
2023-02-21 11:01:57 +0530

    Update TrafficLight.java

--- 13 Traffic Light/TrafficLight.java
+++ 13 Traffic Light/TrafficLight.java
@@ -2,9 +2,39 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class TrafficLight extends JPanel implements ActionListener {
+public class TrafficLight implements ActionListener {
+
+    JPanel panel;
+    JRadioButton red, yellow, green;
+    Color c_red, c_yellow, c_green;
+
+    TrafficLight() {
+        panel = new JPanel();
+        panel.setBounds(0, 0, 200, 200);
+
+        red = new JRadioButton("Red");
+        red.setBounds(0, 0, 100, 50);
+        red.addActionListener(this);
+
+        yellow = new JRadioButton("Yellow");
+        yellow.setBounds(0, 50, 100, 50);
+        yellow.addActionListener(this);
+
+        green = new JRadioButton("Green");
+        green.setBounds(0, 100, 100, 50);
+        green.addActionListener(this);
+
+        panel.add(red);
+        panel.add(yellow);
+        panel.add(green);
+
+    }
 
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public static void main(String[] args) {
+        new TrafficLight();
     }
 }
