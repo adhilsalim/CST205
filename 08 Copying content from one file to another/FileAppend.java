e19b269a66da731642d528cab155b2385726df39
Adhil Salim <bizadhil@gmail.com>
2023-02-23 16:00:49 +0530

    Update FileAppend.java

--- 08 Copying content from one file to another/FileAppend.java
+++ 08 Copying content from one file to another/FileAppend.java
@@ -10,15 +10,26 @@ public class FileAppend {
         FileInputStream fin;
         FileOutputStream fout;
 
+        String temp="";
+
         System.out.println("Enter the name of the file to read [excluding extension]: ");
         String fRead = scan.nextLine();
- 
-        //skipping new line
-        scan.nextLine();
 
+        System.out.println("Enter the name of the file to append [excluding extension]: ");
+        String fAppend = scan.nextLine();
+
+        scan.close();
+
+        try {
             fin = new FileInputStream(fRead + ".txt");
             fout = new FileOutputStream(fAppend + ".txt");
-        }catch(Exception err){}
+
+            int c = fin.read();
+
+          }  
+        catch(Exception err){
+            System.out.println(err.getMessage());
+        }
 
     }
 }
