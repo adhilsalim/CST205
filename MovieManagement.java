23ced5016654ec0747bece95edcf031d73d19e32
Adhil Salim <bizadhil@gmail.com>
2023-02-23 10:21:00 +0530

    Create MovieManagement.java

---
+++ MovieManagement.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+//INCOMPLETE WORKING ON IT
+class MovieDetails {
+    int movieId;
+    String movieName;
+    String cast[];
+    int year;
+}
+
+public class MovieManagement {
+
+    public static void main(String[] args) {
+
+    }
+}
