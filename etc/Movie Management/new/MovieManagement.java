0a173f52abc3dc290aedee3b5a114f91e49770e0
Adhil Salim <bizadhil@gmail.com>
2023-02-23 20:32:41 +0530

    Update MovieManagement.java

--- etc/Movie Management/new/MovieManagement.java
+++ etc/Movie Management/new/MovieManagement.java
@@ -1,11 +1,21 @@
 import java.util.*;
 
+import javax.swing.event.MouseInputAdapter;
+
+//MOVIE DETAILS CLASS
 class MovieDetails {
     int movieId;
     String movieName;
     String cast[];
     int year;
     MovieDetails md;
+}
+
+// THEATRE DETAILS CLASS
+class TheatreDetails {
+    int theatreId;
+    String theatreName;
+    MovieDetails movie;
 }
 
 public class MovieManagement {
@@ -33,24 +43,24 @@ public class MovieManagement {
 
             System.out.print("ENTER CAST NUMBER FOR MOVIE : ");
             int md_castNumber = scan.nextInt();
-            md.cast = new String[md_castNumber];
-            // String cast[] = new String[md_castNumber];
 
+            md.cast = new String[md_castNumber];
             for (int j = 0; j < md_castNumber; j++) {
                 System.out.print("ENTER CAST " + (j + 1) + " FOR MOVIE : ");
-                scan.nextLine();
-                md.cast[j] = scan.nextLine();
+                // scan.nextLine();
+                md.cast[j] = scan.next();
             }
 
             System.out.println("ENTER YEAR OF RELEASE FOR MOVIE : ");
             md.year = scan.nextInt();
-
             movieList.add(md);
         }
         mm.displayMovieDetails();
+        mm.deleteMovie(2);
         scan.close();
     }
 
+    // DISPLAYING MOVIE DETAILS
     public void displayMovieDetails() {
         for (MovieDetails md : movieList) {
             System.out.println("MOVIE ID : " + md.movieId);
@@ -58,5 +68,24 @@ public class MovieManagement {
             System.out.println("MOVIE CAST : " + Arrays.toString(md.cast));
             System.out.println("MOVIE YEAR : " + md.year);
         }
+    }
+
+    // DELETE MOVIE BASED ON MOVIE ID
+    public void deleteMovie(int movieId) {
+        for (MovieDetails md : movieList) {
+            if (md.movieId == movieId) {
+                movieList.remove(md);
+            }
+        }
+    }
+
+    // SORT MOVIE DETAILS BASED ON MOVIE YEAR
+    public void sortMovieDetails() {
+        Collections.sort(movieList, new Comparator<MovieDetails>() {
+            @Override
+            public int compare(MovieDetails md1, MovieDetails md2) {
+                return md1.year - md2.year;
+            }
+        });
     }
 }
