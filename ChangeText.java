314e6e1c2fa4c946d198fbc3362cc5b10e77728e
Adhil Salim <bizadhil@gmail.com>
2023-02-23 09:48:16 +0530

    Update ChangeText.java

--- ChangeText.java
+++ ChangeText.java
@@ -4,7 +4,41 @@ import java.awt.event.*;
 
 public class ChangeText implements ActionListener {
 
+    JFrame frame;
+    JLabel label;
+    JTextField textBox;
+    JRadioButton upperCase, lowerCase;
+    JButton button;
+
     public ChangeText() {
+
+        frame = new JFrame("Change Text");
+        label = new JLabel("Enter your text in the text box");
+        textBox = new JTextField();
+        upperCase = new JRadioButton("uppercase");
+        lowerCase = new JRadioButton("lowercase");
+        button = new JButton("GO!");
+
+        label.setBounds(10, 10, 400, 200);
+        textBox.setBounds(10, 410, 200, 30);
+        upperCase.setBounds(10, 450, 50, 30);
+        lowerCase.setBounds(70, 450, 50, 30);
+        button.setBounds(10, 490, 60, 45);
+
+        frame.add(label);
+        frame.add(textBox);
+        frame.add(upperCase);
+        frame.add(lowerCase);
+        frame.add(button);
+
+        ButtonGroup group = new ButtonGroup();
+        group.add(upperCase);
+        group.add(lowerCase);
+
+        frame.setSize(500, 600);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 
