bab06f5f0c9f4407f99a4097b099a9480bb7876c
Adhil Salim <bizadhil@gmail.com>
2023-02-23 19:37:59 +0530

    Update ThreadCounter.java

--- 09 Thread/ThreadCounter.java
+++ 09 Thread/ThreadCounter.java
@@ -1,45 +1,35 @@
 class Counter {
-    public static int count = 0;
+    int count;
 
-    public static void increment() {
+    public void increment() {
         count++;
-    }
-}
-
-class NoSyncThread implements Runnable {
-    public void run() {
-        for (int i = 0; i < 500; i++) {
-            Counter.increment();
-        }
-    }
-}
-
-class SyncThread implements Runnable {
-    public void run() {
-        for (int i = 0; i < 500; i++) {
-            Counter.increment();
-        }
     }
 }
 
 public class ThreadCounter {
     public static void main(String[] args) {
-        NoSyncThread nst = new NoSyncThread();
-        SyncThread st = new SyncThread();
+        Counter c = new Counter();
 
-        Thread t1 = new Thread(nst, "one");
-        Thread t2 = new Thread(nst, "two");
-        Thread t3 = new Thread(st, "three");
-        Thread t4 = new Thread(st, "four");
+        Thread t1 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 500; i++) {
+                    c.increment();
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 500; i++) {
+                    c.increment();
+                }
+            }
+        });
 
         t1.start();
         t2.start();
-        System.out.println("sum without sync: " + Counter.count);
 
-        Counter.count = 0;
+        System.out.println("Count Value: " + c.count);
 
-        t3.start();
-        t4.start();
-        System.out.println("sum with sync: " + Counter.count);
     }
 }
