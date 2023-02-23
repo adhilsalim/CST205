84fcb7bde24010028d788e87acbfcc7a98e9d0f2
Adhil Salim <bizadhil@gmail.com>
2023-02-23 11:48:50 +0530

    Update MovieManagement.java

--- MovieManagement.java
+++ MovieManagement.java
@@ -1,6 +1,8 @@
 import java.util.*;
 
-//INCOMPLETE WORKING ON IT
+/*
+ * THIS PROGRAM IS NOT COMPLETE [WORKING ON IT]
+ */
 class MovieDetails {
     int movieId;
     String movieName;
@@ -20,23 +22,21 @@ public class MovieManagement {
 
         MovieDetails movie[] = new MovieDetails[total_movies];
 
-        MovieDetails md1 = new MovieDetails();
-        MovieDetails md2 = new MovieDetails();
-        movie[0] = md1;
-        movie[1] = md2;
-
         for (int i = 0; i < movie.length; i++) {
+            movie[i] = new MovieDetails();
 
             System.out.print("ENTER MOVIE ID: ");
             movie[i].movieId = scan.nextInt();
 
             System.out.print("ENTER MOVIE NAME: ");
-            movie[i].movieName = scan.nextLine();
+            String md_movieName = scan.next();
+            movie[i].movieName = md_movieName;
 
             System.out.print("ENTER MOVIE CAST NUMBER: ");
             int md_castNumber = scan.nextInt();
 
             for (int j = 0; j < md_castNumber; j++) {
+
                 System.out.print("ENTER CAST " + (j + 1) + ": ");
                 movie[i].cast[j] = scan.nextLine();
             }
@@ -45,16 +45,17 @@ public class MovieManagement {
             movie[i].year = scan.nextInt();
         }
 
-        for (int i = 0; i < movie.length && movie[i].movieId != -1; i++) {
-            System.out.println("MOVIE ID: " + movie[i].movieId + " MOVIE NAME: " + movie[i].movieName);
-            System.out.print("CAST: ");
+        // for (int i = 0; i < movie.length && movie[i].movieId != -1; i++) {
+        // System.out.println("MOVIE ID: " + movie[i].movieId + " MOVIE NAME: " +
+        // movie[i].movieName);
+        // System.out.print("CAST: ");
 
-            for (int j = 0; j < movie[i].cast.length; j++) {
-                System.out.print(movie[i].cast[j] + ", ");
-            }
+        // for (int j = 0; j < movie[i].cast.length; j++) {
+        // System.out.print(movie[i].cast[j] + ", ");
+        // }
 
-            System.out.println("MOVIE YEAR: " + movie[i].year);
-        }
+        // System.out.println("MOVIE YEAR: " + movie[i].year);
+        // }
 
         scan.close();
     }
