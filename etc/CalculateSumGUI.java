9eaaa585fd924faf730137b13b217cec7d9e255b
Adhil Salim <bizadhil@gmail.com>
2023-02-23 08:19:07 +0530

    Update CalculateSumGUI.java

---
+++ etc/CalculateSumGUI.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class CalculateSumGUI implements ActionListener {
+
+    public CalculateSumGUI() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new CalculateSumGUI();
+    }
+}
