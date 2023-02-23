4f9472ca52120de6060d9c839f38574bee8d0445
Adhil Salim <bizadhil@gmail.com>
2023-02-23 09:29:41 +0530

    Create ChangeText.java

---
+++ ChangeText.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ChangeText implements ActionListener {
+
+    public ChangeText() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new ChangeText();
+    }
+}
