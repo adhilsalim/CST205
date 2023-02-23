44550a8f3690533570182b0ef10b4a2dc0a5a80a
Adhil Salim <bizadhil@gmail.com>
2023-02-23 10:40:12 +0530

    Update MovieManagement.java

--- MovieManagement.java
+++ MovieManagement.java
@@ -18,6 +18,25 @@ public class MovieManagement {
 
         MovieDetails movie[] = new MovieDetails[total_movies];
 
+        for (int i = 0; i < movie.length; i++) {
+            System.out.print("ENTER MOVIE ID: ");
+            movie[i].movieId = scan.nextInt();
+
+            System.out.print("ENTER MOVIE NAME: ");
+            movie[i].movieName = scan.nextLine();
+
+            System.out.print("ENTER MOVIE CAST NUMBER: ");
+            int md_castNumber = scan.nextInt();
+
+            for (int j = 0; j < md_castNumber; j++) {
+                System.out.print("ENTER CAST " + (j + 1) + ": ");
+                movie[i].cast[j] = scan.nextLine();
+            }
+
+            System.out.print("ENTER MOVIE YEAR: ");
+            movie[i].year = scan.nextInt();
+        }
+
         scan.close();
     }
 }
